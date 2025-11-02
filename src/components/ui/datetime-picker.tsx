5065ec6628937401f1a3314100f5336acd113957
Nadir Seghir <nadir.seghir@gmail.com>
2025-11-02 23:34:17 +0100

    use datetime component

--- src/components/ui/datetime-picker.tsx
+++ src/components/ui/datetime-picker.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { CalendarIcon } from 'lucide-react'
+import { CalendarIcon, XIcon } from 'lucide-react'
 import { format } from 'date-fns'
 
 import { cn } from '@/lib/utils'
